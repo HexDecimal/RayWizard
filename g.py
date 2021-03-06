4e0f8038b6f88812406f340edbf80d65c043472c
Kyle Benesch <4b796c65+github@gmail.com>
2021-03-06 01:33:22 -0800

    Add special globals module.
    Disallow untyped definitions.
    
    Organize imports from VS Code.
    
    Enable all runtime warnings.

---
+++ g.py
@@ -0,0 +1,7 @@
+"""Shorthand globals module.  For sharing mutable objects across modules.
+
+This is for organization and has no special effects.
+"""
+import tcod
+
+context: tcod.context.Context  # Global context
--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
+"""Main module.
+
+Run this module from Python to start this program.
+"""
+import sys
+import warnings
+
 import tcod
+
+import g
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+CONSOLE_WIDTH = SCREEN_WIDTH // 12
+CONSOLE_HEIGHT = SCREEN_HEIGHT // 12
 
 
 def main() -> None:
     """Main entrypoint."""
     tileset = tcod.tileset.load_tilesheet("Alloy_curses_12x12.png", 16, 16, tcod.tileset.CHARMAP_CP437)
-    console = tcod.console.Console(1280 // 12, 720 // 12, order="C")
-    with tcod.context.new(width=1280, height=720, tileset=tileset) as context:
+    with tcod.context.new(width=SCREEN_WIDTH, height=SCREEN_HEIGHT, tileset=tileset) as g.context:
         while True:
+            # Rendering.
+            console = tcod.console.Console(CONSOLE_WIDTH, CONSOLE_HEIGHT, order="C")
             console.print(0, 0, "Hello World")
-            context.present(console, integer_scaling=True)
+            g.context.present(console, integer_scaling=True)
+            # Handle input.
             for event in tcod.event.wait():
                 if isinstance(event, tcod.event.Quit):
                     raise SystemExit()
 
 
 if __name__ == "__main__":
+    if not sys.warnoptions:
+        warnings.simplefilter("default")  # Enable all runtime warnings.
     main()

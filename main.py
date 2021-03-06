b9be189d199984c25a591ec7b5a21b99619a3b76
Kyle Benesch <4b796c65+github@gmail.com>
2021-03-06 01:08:28 -0800

    Add VS Code linting.  Set line width to 120.
    Adds requirements.txt file.

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import tcod
 
 def main() -> None:
     """Main entrypoint."""
-    tileset = tcod.tileset.load_tilesheet(
-        "Alloy_curses_12x12.png", 16, 16, tcod.tileset.CHARMAP_CP437
-    )
+    tileset = tcod.tileset.load_tilesheet("Alloy_curses_12x12.png", 16, 16, tcod.tileset.CHARMAP_CP437)
     console = tcod.console.Console(1280 // 12, 720 // 12, order="C")
     with tcod.context.new(width=1280, height=720, tileset=tileset) as context:
         while True:

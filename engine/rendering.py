3815e522eeb13375aba910c59cb128c60412e031
Kyle Benesch <4b796c65+github@gmail.com>
2021-03-06 10:16:51 -0800

    Separate rendering code from the states.

---
+++ engine/rendering.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+import tcod
+
+import g
+
+UI_SIZE = (12, 12)  # Area reserved for the UI.
+
+
+def render_main(console: tcod.console.Console) -> None:
+    """Rendeer the main view.  With the world tiles, any objects, and the UI."""
+    # Render world tiles.
+    map_ = g.world.map
+    console_shape = (console.width - UI_SIZE[0], console.height - UI_SIZE[1])
+    console.tiles_rgb[: console_shape[0], : console_shape[1]] = ord(" "), 0x40, 0x00  # Clear world area.
+    screen_view, world_view = map_.camera.get_views((map_.width, map_.height), console_shape)
+    console.tiles_rgb["ch"][screen_view] = g.world.map.tiles["ch"][world_view]
+
+    # Render all actors.
+    cam_x, cam_y = map_.camera.get_left_top_pos(console_shape)
+    for actor in g.world.map.actors:
+        x = actor.x - cam_x
+        y = actor.y - cam_y
+        if 0 <= x < console_shape[0] and 0 <= y < console_shape[1]:
+            console.print(x, y, "@", fg=(0xFF, 0xFF, 0xFF))
--- engine/states.py
+++ engine/states.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import tcod
 
 import engine.actions
+import engine.rendering
 import g
 from engine.state import State  # Import-time requirement, so `from x import Y` is used.
 
@@ -19,19 +20,7 @@ class InGame(State):
     """The normal in-game state.  Where the player has control over their own actor object."""
 
     def on_draw(self, console: tcod.console.Console) -> None:
-        # Mostly example code.
-        map_ = g.world.map
-        UI_SIZE = (12, 12)  # Area reserved for the UI.
-        console_shape = (console.width - UI_SIZE[0], console.height - UI_SIZE[1])
-        console.tiles_rgb[: console_shape[0], : console_shape[1]] = ord(" "), 0x40, 0x00  # Clear world area.
-        screen_view, world_view = map_.camera.get_views((map_.width, map_.height), console_shape)
-        console.tiles_rgb["ch"][screen_view] = g.world.map.tiles["ch"][world_view]
-        cam_x, cam_y = map_.camera.get_left_top_pos(console_shape)
-        for actor in g.world.map.actors:  # Render all actors.
-            x = actor.x - cam_x
-            y = actor.y - cam_y
-            if 0 <= x < console_shape[0] and 0 <= y < console_shape[1]:
-                console.print(x, y, "@", fg=(0xFF, 0xFF, 0xFF))
+        engine.rendering.render_main(console)
 
     def cmd_move(self, x: int, y: int) -> None:
         if engine.actions.MoveAction(g.world.player, (x, y)).perform():

aa87cc3da4ac8dcc2355387abebcc24403c1762e
Kyle Benesch <4b796c65+github@gmail.com>
2021-03-07 06:27:51 -0800

    Automatically apply effects to actors.

--- engine/effects.py
+++ engine/effects.py
@@ -7,16 +7,19 @@ import g
 
 class Effect:
     def apply(self, x: int, y: int) -> None:
-        pass
+        for actor in [actor for actor in g.world.map.actors if x == actor.x and y == actor.y]:
+            actor.apply_effect(self)
 
 
 class Cold(Effect):
     def apply(self, x: int, y: int) -> None:
+        super().apply(x, y)
         if g.world.map.tiles[x, y] == engine.tiles.WATER.as_np():
             g.world.map.tiles[x, y] = engine.tiles.ICE_FLOOR
 
 
 class Heat(Effect):
     def apply(self, x: int, y: int) -> None:
+        super().apply(x, y)
         if g.world.map.tiles[x, y] == engine.tiles.ICE_FLOOR.as_np():
             g.world.map.tiles[x, y] = engine.tiles.WATER

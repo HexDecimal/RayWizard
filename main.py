262b3859feecff1b83b64442dafc00c96e092271
Kyle Benesch <4b796c65+github@gmail.com>
2021-03-12 12:40:03 -0800

    Add level select debug option.
    Set the environment flag LEVEL to the level you want to test.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ Run this module from Python to start this program.
 from __future__ import annotations  # This may be required to resolve import order issues.
 
 import logging
+import os
 import sys
 import warnings
 
@@ -22,8 +23,11 @@ def main() -> None:
     tileset = tcod.tileset.load_tilesheet("Alloy_curses_12x12.png", 16, 16, tcod.tileset.CHARMAP_CP437)
     with tcod.context.new(width=SCREEN_WIDTH, height=SCREEN_HEIGHT, tileset=tileset) as g.context:
         g.world = engine.world.World()
+        level = 1
+        if __debug__:
+            level = int(os.environ.get("LEVEL", level))
 
-        g.world.map = procgen.dungeon.generate(g.world, level=1)
+        g.world.map = procgen.dungeon.generate(g.world, level=level)
 
         g.world.loop()
 
